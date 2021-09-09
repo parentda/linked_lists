57d11f9c1ea92cbf0ce3b9d480539b85c3e1e91c
parentda <danielparent21@gmail.com>
2021-09-09 14:27:38 -0400

    added shells for all applicable methods

--- linked_list.rb
+++ linked_list.rb
@@ -1,2 +1,27 @@
 class LinkedList
+  def initialize; end
+
+  def append(value); end
+
+  def prepend(value); end
+
+  def size; end
+
+  def head; end
+
+  def tail; end
+
+  def at(index); end
+
+  def pop; end
+
+  def contains?(value); end
+
+  def find(value); end
+
+  def to_s; end
+
+  def insert_at(value, index); end
+
+  def remove_at(value, index); end
 end
